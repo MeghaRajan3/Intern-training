0e6da73d546d96d10950acb7d3070218d628ef37
Manimegalai <smmegalai94@gmail.com>
2025-01-24 17:02:26 +0530

    first

---
+++ Manimegalai Training/Jan 24 - If condition | loop/password.js
@@ -0,0 +1,10 @@
+let userName="megha";
+let password="user123";
+let role="Admin";
+ if(userName === "jacob" && password === "Hello@123")  {
+    console.log("You are logged in")
+    role=="Admin"?console.log("you are admin"):console.log("you are not admin")
+ } else {
+    console.log("Invalid Username and Password")
+ }
+ 
