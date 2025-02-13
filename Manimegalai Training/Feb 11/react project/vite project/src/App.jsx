4ca3763b8bf30044a231132e634ee5b911d7b3c3
Manimegalai <smmegalai94@gmail.com>
2025-02-13 09:45:40 +0530

    login form

---
+++ Manimegalai Training/Feb 11/react project/vite project/src/App.jsx
@@ -0,0 +1,14 @@
+import LearnComponents from "./components/learnComponents";
+
+function App(){
+return (
+  <div>
+    <div>React project</div>
+<LearnComponents/>
+  </div>
+  
+      
+  );
+}
+
+export default App
